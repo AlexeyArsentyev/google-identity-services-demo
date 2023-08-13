5770f1a1a5fa631f114944a2e9dbc6eee70605bd
AlexeyArsentyev <alexey10.arsentyev@gmail.com>
2023-08-13 17:43:33 +0300

    basic stuff

--- src/App.js
+++ src/App.js
@@ -1,49 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-import {GoogleLogin} from '@react-oauth/google';
+import "./App.css";
+import { GoogleLogin } from "@react-oauth/google";
 import jwt_decode from "jwt-decode";
-import {useGoogleLogin} from '@react-oauth/google';
-import axios from "axios"
+import { useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function App() {
+  const login = useGoogleLogin({
+    onSuccess: async (respose) => {
+      try {
+        const res = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
+          headers: {
+            Authorization: `Bearer ${respose.access_token}`,
+          },
+        });
 
-    const login = useGoogleLogin({
-        onSuccess: async respose => {
-            try {
-                const res = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
-                    headers: {
-                        "Authorization": `Bearer ${respose.access_token}`
-                    }
-                })
+        console.log(res.data, respose.access_token);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  });
 
-                console.log(res.data)
-            } catch (err) {
-                console.log(err)
-
-            }
-
-        }
-    });
-
-    return (
-        <div className="App">
-            <header className="App-header">
-                <button onClick={login}>
-                    <i class="fa-brands fa-google"></i>
-                    Continue with google
-                </button>
-                <GoogleLogin
-                    onSuccess={credentialResponse => {
-                    console.log(credentialResponse.credential);
-                    var decoded = jwt_decode(credentialResponse.credential);
-                    console.log(decoded)
-                }}
-                    onError={() => {
-                    console.log('Login Failed');
-                }}/>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <GoogleOAuthProvider clientId="217838202385-uad8jqp1b8b35crn0k2du5dhbkpp9hs7.apps.googleusercontent.com">
+        <button onClick={login} className="googleBtn">
+          <img src="googleLogo.png" alt="" />
+        </button>
+      </GoogleOAuthProvider>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {GoogleOAuthProvider} from '@react-oauth/google';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-    <React.StrictMode>
-        <GoogleOAuthProvider clientId="YOURCLIENT_ID">
-            <App/></GoogleOAuthProvider>;
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />;
+  </React.StrictMode>
 );
 
 reportWebVitals();
